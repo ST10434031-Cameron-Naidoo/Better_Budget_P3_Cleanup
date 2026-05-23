f1fe3b720051029dfc319500938472a430815e75
St10439633-Jordan-Harding <163293676+St10439633-Jordan-Harding@users.noreply.github.com>
2026-05-23 10:49:42 +0200

    Connected Firebase to application

--- prog7313-part-2-bestbudget-master/app/build.gradle.kts
+++ prog7313-part-2-bestbudget-master/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 android {
     namespace = "com.dachkaboiz.betterbudget_bestbudget"
@@ -51,6 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     // Room
     implementation("androidx.room:room-runtime:2.7.1")
