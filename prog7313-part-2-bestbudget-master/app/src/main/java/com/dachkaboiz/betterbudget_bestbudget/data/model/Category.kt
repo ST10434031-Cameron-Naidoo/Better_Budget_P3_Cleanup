55a500c61fdbc978f8cbd74bca25a95ea5cda8ab
Saxola <st10057997@vcconnect.edu.za>
2026-05-24 15:41:00 +0200

    Migrated Category from Room entity to Firebase data class Removed all Room annotations (@Entity, @PrimaryKey, @ForeignKey, @Index). Replaced categoryID: Int with firebaseId: String to align with Firebase Realtime Database push key format. Added default values to all fields as required for Firebase deserialization via getValue(Category::class.java).

--- prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/data/model/Category.kt
+++ prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/data/model/Category.kt
@@ -5,21 +5,10 @@ import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(
-    tableName = "categories",
-    indices = [Index(value = ["userEmail"])],
-    foreignKeys = [ForeignKey(
-        entity = User::class,
-        parentColumns = ["email"],
-        childColumns = ["userEmail"],
-        onDelete = ForeignKey.CASCADE
-    )]
-)
 data class Category(
-    @PrimaryKey(autoGenerate = true)
-    val categoryID: Int = 0,
-    val userEmail: String,
-    val categoryName: String,
-    val categoryIcon: String,
-    val categoryDescription: String
+    val firebaseId: String = "",
+    val userEmail: String = "",
+    val categoryName: String = "",
+    val categoryIcon: String = "",
+    val categoryDescription: String = ""
 )
