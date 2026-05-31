dd4c0032db828f5a0a981ea25a0518495529e15c
Cameron Naidoo <st10434031@myemeris.edu.za>
2026-05-31 21:38:46 +0200

    modified SubCategoryGoal model to include firebase ids

--- prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/data/model/SubCategoryGoal.kt
+++ prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/data/model/SubCategoryGoal.kt
@@ -4,12 +4,10 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
-
 data class SubCategoryGoal(
-    val subCategoryID: String = "",
-    val categoryID: String = "",
-//    val firebaseSubCategoryId: String = "",      // links to SubCategory.firebaseId
-//    val firebaseParentCategoryId: String = "",   // links to Category.firebaseId
+    val firebaseId: String = "",
+    val subCategoryFirebaseId: String = "",
+    val categoryFirebaseId: String = "",
     val minGoal: Double? = null,
     val maxGoal: Double? = null,
     val month: Int = 0,
