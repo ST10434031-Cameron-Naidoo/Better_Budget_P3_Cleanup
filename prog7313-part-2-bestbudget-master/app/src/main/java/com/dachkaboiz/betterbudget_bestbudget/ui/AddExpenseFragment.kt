00a5d731483bf673e36a0f520e495ec1e56a52b2
St10439633-Jordan-Harding <163293676+St10439633-Jordan-Harding@users.noreply.github.com>
2026-05-31 18:06:07 +0200

    Added automate view to edit expense layout file

--- prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/ui/AddExpenseFragment.kt
+++ prog7313-part-2-bestbudget-master/app/src/main/java/com/dachkaboiz/betterbudget_bestbudget/ui/AddExpenseFragment.kt
@@ -20,7 +20,7 @@ import kotlinx.coroutines.launch
 import java.io.File
 import java.util.Calendar
 
-class AddExpenseFragment : Fragment(R.layout.fragment_add_expense) {
+class AddExpenseFragment : Fragment(R.layout.fragment_add_expense_v2) {
 
     private lateinit var repository: ExpenseRepository
     private val firebaseCategoryRepository = FirebaseCategoryRepository()
